4bc171dfac68b619edaa0408d4c4e0d4981f0ad6
Stephen Malito <stevemalito@gmail.com>
2019-07-22 17:53:23 -0500

    Submit button grabing articles but not populating data yet

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,43 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  }
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+$("#submit").on("click", function() {
+
+  axios.get("http://scca-chicago.com/calendar/").then(function(response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+      
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("div.summary").each(function(i, element) {
+          // Save an empty result object
+          var result = {};
+    
+          // Add the text and href of every link, and save them as properties of the result object
+          result.title = $(this)
+            .children("a")
+            .text();
+          result.link = $(this)
+            .children("a")
+            .attr("href");
+    
+          // Create a new Article using the `result` object built from scraping
+          db.Article.create(result)
+            .then(function(dbArticle) {
+              // View the added result in the console
+              console.log(dbArticle);
+            })
+            .catch(function(err) {
+              // If an error occurred, log it
+              console.log(err);
+            });
+        });
+    
+        // Send a message to the client
+        res.send("Scrape Complete");
+      });
+    // });
+    
 });
 
 
